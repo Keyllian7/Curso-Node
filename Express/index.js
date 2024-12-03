c8f66458d765dc628e4621fec58709155b266feb
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-02 22:10:45 -0300

    Express, parâmetros e nodemon

---
+++ Express/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+
+
+app.get("/", function(req, res){
+    res.send("Inicio")
+})
+
+app.get("/sobre", function(req, res){
+    res.send("Sobre")
+})
+
+app.get("/info", function(req, res){
+    res.send("Info")
+})
+
+app.get("/parm/:nome/:cargo/:idade", function(req, res){
+    res.send(
+        "<h1>Oi " + req.params.nome + "</h1>"+
+        "<h2>Seu cargo é: " + req.params.cargo + "</h2>"+ 
+        "<h3>Sua idade é: " + req.params.idade + "</h3>"
+        )
+})
+
+app.listen(8081, function(){
+    console.log("Servidor online!")
+})
