9d1feff98bacbeba23e9ddd33c2b9d626e95d8fc
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-03 23:43:06 -0300

    Models no sequelize

--- Sequelize/app.js
+++ Sequelize/app.js
@@ -6,6 +6,47 @@ const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.SE
     dialect: "mysql"
 })
 
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+
+})
+
+// Postagem.sync({force: true})
+// Usuario.sync({force: true})
+
+
+Postagem.create({
+    titulo: "Um titulo qualquer",
+    conteudo: "Um conteudo qualquer"
+})
+
+Usuario.create({
+    nome: "Keyllian",
+    sobrenome: "Azevedo",
+    idade: 18,
+    email: "keyllian@exemplo.com"
+})
+
 sequelize.authenticate().then(function(){
     console.log("Conectado com sucesso!")
 }).catch(function(erro){
