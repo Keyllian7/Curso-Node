445d49c23177d38051820231dce79ce4bf9a54cf
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-04 19:25:13 -0300

    Coletando dados e adicionando body parse

--- Handlebars/app.js
+++ Handlebars/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 // Configurações
 
@@ -9,6 +10,11 @@ const handlebars = require('express-handlebars');
 
         app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
         app.set('view engine', 'handlebars');
+
+    // Body Parser
+    
+        app.use(bodyParser.urlencoded({extended: false}));
+        app.use(bodyParser.json());
 
     // Conexão com o banco de dados
 
@@ -26,16 +32,13 @@ const handlebars = require('express-handlebars');
 // Rotas
 
         app.get('/cadastro', function(req, res){
+            req.body.titulo
             res.render('form')
         })
 
-
-
-
-
-
-
-
+        app.post('/adicionar', function(req, res){
+            res.send('Texto:' + req.body.titulo + " " + 'Conteudo:' + req.body.conteudo)
+        })
 
 app.listen(8081, function(){
     console.log('Servidor online! http://localhost:8081');
