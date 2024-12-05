aaad26012f3ed0e147e2b0530e1c8efb774c1bcd
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-05 10:38:17 -0300

    coletando informações e redirecionando usuário

--- Handlebars/app.js
+++ Handlebars/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const Post = require('./models/post');
 
 // Configurações
     // Template Engine
@@ -13,13 +14,26 @@ const bodyParser = require('body-parser');
         app.use(bodyParser.json());
 
 // Rotas
+
+        app.get("/", function(req, res){
+            res.render('home')
+        })
+
         app.get('/cadastro', function(req, res){
             req.body.titulo
             res.render('form')
         })
 
         app.post('/adicionar', function(req, res){
-            res.send('Texto:' + req.body.titulo + " " + 'Conteudo:' + req.body.conteudo)
+
+            Post.create({
+                titulo: req.body.titulo,
+                conteudo: req.body.conteudo
+            }).then(function(){
+                res.redirect('/')
+            }).catch(function(erro){
+                res.send("Erro ao criar o post: " + erro)
+            })
         })
 
 app.listen(8081, function(){
