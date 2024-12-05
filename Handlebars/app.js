983109b46d4616d600b9bbb6fbcb409426c61970
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-05 12:22:00 -0300

    Excluindo postagem

--- Handlebars/app.js
+++ Handlebars/app.js
@@ -39,6 +39,14 @@ const Post = require('./models/post');
             })
         })
 
+        app.get('/deletar/:id', function(req, res){
+            Post.destroy({where: {'id': req.params.id}}).then(function(){
+                res.send("Postagem deletada com sucesso!")
+            }).catch(function(erro){
+                res.send("Erro ao deletar postagem: " + erro)
+            })
+        })
+
 app.listen(8081, function(){
     console.log('Servidor online! http://localhost:8081');
 });
