ec41d469e7715709ddeeb8a0c35d6f9d405c33b1
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-06 12:19:19 -0300

    Criando collection e adicionando dados no mongo

--- mongo-trabalhando/aprendendo.js
+++ mongo-trabalhando/aprendendo.js
@@ -2,10 +2,53 @@ const mongoose = require('mongoose');
 
 // Conectando e configurando o mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/cursodb", {
-    useMongoClient: true
+mongoose.connect("mongodb://localhost:27017/cursodb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 }).then(() => {
     console.log("Conectado ao banco de dados");
 }).catch((err) => {
     console.log("Erro ao conectar ao banco de dados " + err);
 })
+
+// Models - Definindo
+const UsuarioSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String,
+        require: false
+    }
+});
+
+// Collection
+mongoose.model('usuario', UsuarioSchema);
+
+const NewUsuario = mongoose.model('usuario');
+
+// Salvando dados
+new NewUsuario({
+    nome: "Lucas",
+    sobrenome: "Anderson",
+    email: "lucas@example.com",
+    idade: 20,
+    pais: "China"
+}).save().then(() => {
+    console.log("Usuário criado com sucesso");
+}).catch((err) => {
+    console.log("Erro ao criar usuário " + err);
+});
